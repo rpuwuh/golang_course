e822fd0e6811d0be1253bae9522f175317195d91
mdgrishin <mdgrishin@hex.team>
2024-10-01 13:20:14 +0300

    Added 12.3 exercise

---
+++ 1/1.12/3.go
@@ -0,0 +1,36 @@
+/*
+	На ввод подаются пять целых чисел,
+	которые записываются в массив. Однако эта часть программы уже написана.
+	Вам нужно написать фрагмент кода,
+		с помощью которого можно найти и вывести максимальное число в этом массиве.
+	Sample Input:
+		2
+		3
+		56
+		45
+		21
+	Sample Output:
+		56
+*/
+
+package main
+import "fmt"
+
+func main()  {
+	array := [5]int{}
+	var a int
+	for i:=0; i < 5; i++{
+		fmt.Scan(&a)
+		array[i] = a
+	}
+    // здесь ваш код
+    // ...
+
+	var max int = array[0]
+	for i:=0; i < 5; i++{
+		if array[i] > max {
+			max = array[i]
+		}
+	}
+	fmt.Println(max)
+}
